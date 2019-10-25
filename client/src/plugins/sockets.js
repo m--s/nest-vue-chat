e7b6b6e11476482bd085c49bc3138278a0fa3051
Marcin Sobczak <marcin@sobczak.me>
2019-10-25 18:44:40 +0200

    Support for dotenv variables

--- client/src/plugins/sockets.js
+++ client/src/plugins/sockets.js
@@ -2,6 +2,6 @@ import VueSocketIOExt from 'vue-socket.io-extended';
 import io from 'socket.io-client';
 import Vue from 'vue';
 
-const socket = io('localhost:3000');
+const socket = io(process.env.VUE_APP_WS_ADDRESS);
 
 Vue.use(VueSocketIOExt, socket);
